a5e2e61e20f8110716ef2b230527f95acdf18a65
Elham Bashari <elhambashari84@gmail.com>
2025-05-08 20:09:33 +0200

    productstore.js created

---
+++ src/store/ProductStore.js
@@ -0,0 +1,21 @@
+
+
+
+import { create } from 'zustand';
+
+export const useProductStore = create((set) => ({
+  products: [],
+  setProducts: (products) => set({ products }),
+  addProduct: (product) =>
+    set((state) => ({ products: [...state.products, product] })),
+  removeProduct: (id) =>
+    set((state) => ({
+      products: state.products.filter((product) => product.id !== id),
+    })),
+  updateProduct: (updatedProduct) =>
+    set((state) => ({
+      products: state.products.map((product) =>
+        product.id === updatedProduct.id ? updatedProduct : product
+      ),
+    })),
+}));
