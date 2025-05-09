cf260e2dde6a326be654e662a4a0a0560ebd4bb9
Elham Bashari <elhambashari84@gmail.com>
2025-05-09 12:17:33 +0200

    App.jsx changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer'; 
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import AdminPanel from './pages/AdminPanel';
+import NotFound from './pages/Notfound';
+import './App.css';
 
 function App() {
-
-
-  return (
-    <>
-    
-    </>
-  )
-}
-
-export default App
+	return (
+	  <div className="app-container">
+		<Navbar />
+		<main className="main-content">
+		  <Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/cart" element={<Cart />} />
+			<Route path="/admin" element={<AdminPanel />} />
+			<Route path="*" element={<NotFound />} />
+		  </Routes>
+		</main>
+		<Footer />
+	  </div>
+	);
+  }
+  
+  export default App;
+	
