a8a8323fca6f6ed173d09a3d57022bc96b7bab56
Elham Bashari <elhambashari84@gmail.com>
2025-05-11 19:21:51 +0200

    footer.jsx changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
 
-
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
+  const navigate = useNavigate();
+
+  const handleAdminClick = () => {
+    navigate("/admin");
+  };
+
   return (
     <footer className="footer">
       <div className="footer-section">
@@ -13,14 +19,11 @@ const Footer = () => {
 
       <div className="footer-section">
         <h4>Butik address:</h4>
-        <p>Södra hamngatan 8</p>
+        <p>Södra hamngatan 8 Göteborg</p>
       </div>
 
       <div className="footer-section">
-        <button
-          className="admin-button"
-          onClick={() => window.location.href = "/admin"}
-        >
+        <button className="admin-button" onClick={handleAdminClick}>
           Admin
         </button>
       </div>
