c7254b188e6fdcb082938850298cbd30db5fac6e
Elham Bashari <elhambashari84@gmail.com>
2025-05-13 09:46:48 +0200

    Notfound.jsx added

---
+++ src/pages/Notfound.jsx
@@ -0,0 +1,13 @@
+import './NotFound.css';
+
+function NotFound() {
+	return (
+	  <div style={{ padding: '2rem', textAlign: 'center' }}>
+		<h1>404 - Sidan hittades inte 🚫</h1>
+		<p>Den begärda sidan finns inte.</p>
+	  </div>
+	);
+  }
+  
+  export default NotFound;
+  
